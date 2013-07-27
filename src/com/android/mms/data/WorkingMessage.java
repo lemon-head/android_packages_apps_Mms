980f726b4f00fb44a4b03b83f6b4616d3a0ca29e
kaiyiz <kaiyiz@codeaurora.org>
2013-07-27 16:31:55 +0800

    MMS: The MMS subject is still exist when launch again
    Mms draft do not delete when convert Mms to Sms.
    
    If convert one Mms Draft to Sms and send it, need delete Mms
    Draft.
    
    CRs-Fixed: 408450
    
    Change-Id: I19188c6e75e2ef25e399b92b5372bcdaf69ef7c1

--- src/com/android/mms/data/WorkingMessage.java
+++ src/com/android/mms/data/WorkingMessage.java
@@ -1333,9 +1333,11 @@ public class WorkingMessage {
             sendSmsWorker(msgText, semiSepRecipients, threadId);
         }
 
-            // Be paranoid and clean any draft SMS up.
-            deleteDraftSmsMessage(threadId);
-        }
+        // Be paranoid and clean any draft SMS up.
+        deleteDraftSmsMessage(threadId);
+
+        // if convert one Mms Draft to Sms and send it,so need delete Mms Draft.
+        deleteDraftMmsMessage(threadId);
     }
 
     private void sendSmsWorker(String msgText, String semiSepRecipients, long threadId) {
@@ -1814,6 +1816,11 @@ public class WorkingMessage {
         SqliteWrapper.delete(mActivity, mContentResolver,
                 ContentUris.withAppendedId(Sms.Conversations.CONTENT_URI, threadId),
                 SMS_DRAFT_WHERE, null);
+    }
+
+    private void deleteDraftMmsMessage(long threadId) {
+        SqliteWrapper.delete(mActivity, mContentResolver,Mms.Draft.CONTENT_URI,
+                Mms.THREAD_ID + " = " +threadId, null);
     }
 
     private void asyncDeleteDraftMmsMessage(Conversation conv) {
