0d0d4dafd45508a3dde92ddb8b33c43244cdfd8c
kaiyiz <kaiyiz@codeaurora.org>
2013-07-29 10:40:06 +0800

    Mms: The original picture can't replace after DUT landscape
    Loading image is asynchronous execution,when call onItemLoaded
    to resotre new image,other onItemLoaded will restore image again
    which hold the old image uri and called by update() method,then
    this CR occurs.
    
    Only when loaded uri is match current ImageModel uri,it is
    allowed to update image.
    
    CRs-Fixed: 427183
    
    Change-Id: Ia8bbd8395ac58c6274f4ffcc567e11a8bcd01864

--- src/com/android/mms/util/ThumbnailManager.java
+++ src/com/android/mms/util/ThumbnailManager.java
@@ -145,6 +145,8 @@ public class ThumbnailManager extends BackgroundLoaderManager {
         if (thumbnailExists) {
             if (callbackRequired && !DEBUG_DISABLE_CALLBACK) {
                 ImageLoaded imageLoaded = new ImageLoaded(thumbnail, isVideo);
+                // save image uri
+                imageLoaded.setUri(uri.toString());
                 callback.onItemLoaded(imageLoaded, null);
             }
             return new NullItemLoadedFuture();
@@ -274,6 +276,8 @@ public class ThumbnailManager extends BackgroundLoaderManager {
                             }
                             if (!DEBUG_DISABLE_CALLBACK) {
                                 ImageLoaded imageLoaded = new ImageLoaded(bitmap, mIsVideo);
+                                // save image uri
+                                imageLoaded.setUri(mUri.toString());
                                 callback.onItemLoaded(imageLoaded, null);
                             }
                         }
@@ -535,10 +539,17 @@ public class ThumbnailManager extends BackgroundLoaderManager {
     public static class ImageLoaded {
         public final Bitmap mBitmap;
         public final boolean mIsVideo;
+        public  String mUri;
 
         public ImageLoaded(Bitmap bitmap, boolean isVideo) {
             mBitmap = bitmap;
             mIsVideo = isVideo;
+        }
+        public void setUri(String uri) {
+            mUri = uri;
+        }
+        public String getUri() {
+            return mUri;
         }
     }
 }
